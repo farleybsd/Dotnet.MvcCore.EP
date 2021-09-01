632e00b050f6d261b8c31a34be1c50d897e2ef64
farleybsd <farley.t.i@hotmail.com>
2021-09-01 00:44:12 -0300

    Entendendo o padrão MVC na prática

---
+++ MinhaDemoMvc/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MinhaDemoMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinhaDemoMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public IActionResult Index(string id , string categoria)
+        {
+            return View();
+        }
+        // Se o Nome da View For o Mesmo Nome do METODO
+        // Nao precisa passar a View como parametro
+        public IActionResult Privacy()
+        {
+            return View();
+            //return View("Privacy");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
