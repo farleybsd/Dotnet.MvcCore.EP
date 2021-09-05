d37057c8b8d244024fedcde2b363cce7a106607a
farleybsd <farley.t.i@hotmail.com>
2021-09-05 01:06:19 -0300

    Trabalhando com DataAnnotations

--- MinhaDemoMvc/Models/Filme.cs
+++ MinhaDemoMvc/Models/Filme.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +9,30 @@ namespace MinhaDemoMvc.Models
 {
     public class Filme
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required(ErrorMessage ="O campo titulo e obrigatorio")]
+        [StringLength(60,MinimumLength =3,ErrorMessage ="O titulo  precisa ter entre 3 ou 60 caracteres")]
         public string Titulo { get; set; }
+
+        [DataType(DataType.DateTime, ErrorMessage="Data em formato incorreto")]
+        [Required(ErrorMessage = "O campo Data De Lancamento e obrigatorio")]
+        [Display(Name = "Data De Lancamento")]
         public DateTime DataLancamento { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\u00C0-\u00FF""'\w-]*$",ErrorMessage ="Genero em formato invalido")]
+        [StringLength(30,ErrorMessage ="M'aximo de 30 caracteres")]
         public string Genero { get; set; }
+
+        [Range(1,1000,ErrorMessage ="Valor de 1 a 1000")]
+        [Required(ErrorMessage = "Preencha o campo valor")]
+        [Column(TypeName ="decimal(18,2)")] // tipo do dado no Banco De Dados
         public decimal Valor { get; set; }
+
+        [Required(ErrorMessage = "Preencha o campo Avaliacao")]
+        [Display(Name = "Avaliacao")]
+        [RegularExpression(@"^[0-5]*$",ErrorMessage ="Somente numeros")]
         public int Avaliacao { get; set; }
     }
 }
