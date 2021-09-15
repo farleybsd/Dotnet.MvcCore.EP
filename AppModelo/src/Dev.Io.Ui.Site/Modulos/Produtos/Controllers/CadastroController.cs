d4d598644cd7baa75b9a2b120692d73b6d56163f
farleybsd <farley.t.i@hotmail.com>
2021-09-15 01:16:19 -0300

    Exemplos de utilização

--- AppModelo/src/Dev.Io.Ui.Site/Modulos/Produtos/Controllers/CadastroController.cs
+++ AppModelo/src/Dev.Io.Ui.Site/Modulos/Produtos/Controllers/CadastroController.cs
@@ -4,12 +4,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Dev.Io.Ui.Site.Areas.Produtos.Controllers
+namespace Dev.Io.Ui.Site.Modulos.Produtos.Controllers
 {
     [Area("Produtos")]
+    [Route("produtos")]
     public class CadastroController : Controller
     {
+        [Route("lista")]
         public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("busca")]
+        public IActionResult Busca()
         {
             return View();
         }
--- AppModelo/src/Dev.Io.Ui.Site/Startup.cs
+++ AppModelo/src/Dev.Io.Ui.Site/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -15,6 +16,13 @@ namespace Dev.Io.Ui.Site
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<RazorViewEngineOptions>(options =>
+            {
+                options.AreaViewLocationFormats.Clear();
+                options.AreaViewLocationFormats.Add("/Modulos/{2}/Views/{1}/{0}.cshtml");
+                options.AreaViewLocationFormats.Add("/Modulos/{2}/Views/Shared/{0}.cshtml");
+                options.AreaViewLocationFormats.Add("/Views/Shared/{0}.cshtml");
+            });
             services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_2);
         }
 
@@ -35,10 +43,13 @@ namespace Dev.Io.Ui.Site
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}"
                     );
-                routes.MapRoute(
-                    name: "areas",
-                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
-                    );
+                //routes.MapRoute(
+                //    name: "areas",
+                //    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                //    );
+
+                routes.MapAreaRoute("AreaProdutos","Produtos", "Produtos/{ controller = Cadastro}/{ action = Index}/{id?}");
+                routes.MapAreaRoute("AreaVendas", "Vendas", "Vendas/{controller=Pedidos}/{ action = Index}/{id?}");
 
             });
         }
